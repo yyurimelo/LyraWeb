163f1e71389be4632614401d0ae117143a8d7afa
yyurimelo <yuri.devsy@gmail.com>
2025-08-21 16:55:33 -0300

    .

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().url(),
+  VITE_API_URL: z.string().url().min(1, "Obrigatório"),
+  GOOGLE_CLIENT_ID: z.string().min(1, "Obrigatório"),
+  GOOGLE_CLIENT_SECRET: z.string().min(1, "Obrigatório"),
 });
 
 export const env = envSchema.parse(import.meta.env);
