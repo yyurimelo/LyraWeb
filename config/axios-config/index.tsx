4f360570d468e820aadf2d83df162e1e19368d08
yuri <yuri.devsy@gmail.com>
2026-01-18 23:44:16 -0300

    .

--- config/axios-config/index.tsx
+++ config/axios-config/index.tsx
@@ -3,7 +3,7 @@ import axios, { type AxiosInstance, isAxiosError } from "axios";
 let http: AxiosInstance;
 
 const createHttp = (baseURL: string) => {
-  http = axios.create({ baseURL, withCredentials: false });
+  http = axios.create({ baseURL, withCredentials: true });
 };
 
 export { createHttp, http, isAxiosError };
